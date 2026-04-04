278543f17c2e4eda7ff9cb1ce5fc4c222842cb3a
nchau11-xiu <nnchau1111@gmail.com>
2026-04-04 21:34:07 +0700

    Add files via upload

---
+++ LichHen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAn_QuanLyBenhVien.Models
+{
+    public class LichHen
+    {
+        public string MaLichHen { get; set; }
+        public string MaBenhNhan { get; set; }
+        public string MaNV { get; set; }
+        public DateTime NgayHen { get; set; }
+        public string LyDoKham { get; set; }
+        public string TrangThai { get; set; }
+
+        // Các thuộc tính liên kết (Navigation Properties)
+        public virtual BenhNhan BenhNhan { get; set; }
+        public virtual NhanVien NhanVien { get; set; }
+    }
+}
