c7d15898bf16a6cf8cf525dd59f5d617cc94f1be
nchau11-xiu <nnchau1111@gmail.com>
2026-04-29 11:15:20 +0700

    Update HoaDon.cs

--- DoAn_QuanLyBenhVien/Models/HoaDon.cs
+++ DoAn_QuanLyBenhVien/Models/HoaDon.cs
@@ -6,15 +6,30 @@ using System.Threading.Tasks;
 
 namespace DoAn_QuanLyBenhVien.Models
 {
-    public class HoaDon
+    public class HoaDon: BaseViewModel
     {
         public string MaHoaDon { get; set; }
         public string MaPhieuKham { get; set; }
         public string MaNV { get; set; }
         public DateTime NgayLap { get; set; }
+        private string _trangThai;
+        public string TrangThai
+        {
+            get => _trangThai;
+            set { _trangThai = value; OnPropertyChanged(); } 
+        }
         public string TrangThai { get; set; }
         public string TenThuNgan { get; set; }
         public string TenBenhNhan { get; set; }
         public decimal TongTien { get; set; }
+    }
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+
     }
 }
